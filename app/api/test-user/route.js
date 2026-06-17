b0229aa2fef838b3bc06743fddd72f45a4127101
Dhanuu <dhananjayshirse7777@gmail.com>
2026-06-17 21:21:33 +0530

    test users

--- app/api/test-user/route.js
+++ app/api/test-user/route.js
@@ -1,19 +1,12 @@
 import connectDB from "@/config/db";
+import mongoose from "mongoose";
 import User from "@/models/User";
 
 export async function GET() {
-  try {
-    await connectDB();
+  await connectDB();
 
-    const users = await User.find();
-
-    return Response.json({
-      count: users.length,
-      users,
-    });
-  } catch (error) {
-    return Response.json({
-      error: error.message,
-    });
-  }
+  return Response.json({
+    dbName: mongoose.connection.db.databaseName,
+    users: await User.countDocuments(),
+  });
 }
