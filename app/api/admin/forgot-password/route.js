0f557bcb9019ded4604c14e8db364ae94871322d
Dhanuu <dhananjayshirse7777@gmail.com>
2026-06-17 21:56:37 +0530

    Fix SMTP

--- app/api/admin/forgot-password/route.js
+++ app/api/admin/forgot-password/route.js
@@ -37,17 +37,15 @@ export async function POST(req) {
 
     await user.save();
 
-    const transporter =
-      nodemailer.createTransport({
-        service: "gmail",
-        auth: {
-          user:
-            process.env.EMAIL_USER,
-          pass:
-            process.env.EMAIL_PASS,
-        },
-      });
-
+    const transporter = nodemailer.createTransport({
+  host: "smtp.gmail.com",
+  port: 587,
+  secure: false,
+  auth: {
+    user: process.env.EMAIL_USER,
+    pass: process.env.EMAIL_PASS,
+  },
+});
     await transporter.sendMail({
       from:
         process.env.EMAIL_USER,
